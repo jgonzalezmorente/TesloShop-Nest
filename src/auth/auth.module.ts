1386f5c4d1ee9280a6bb0ab53b89f77300367a20
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-09-19 09:46:30 +0200

    Nest Authentication - Passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -9,6 +11,15 @@ import { User } from './entities/user.entity';
   providers: [ AuthService ],
   imports: [
     TypeOrmModule.forFeature( [ User ] ),
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    }),
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: {
+    //     expiresIn: '2h'
+    //   }
+    // })
   ],
   exports: [ TypeOrmModule ]
 })
